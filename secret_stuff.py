9119ed77d74bf62512371364b0184cfeeedeb763
Grewoss <Grewoss@gmail.com>
2018-01-01 18:01:38 +0100

    You can't get xp in #spam

--- secret_stuff.py
+++ secret_stuff.py
@@ -1,10 +1,8 @@
 def bot_token():
     # This is just a placeholder token.
-    return "Mzc3OTM1NTQxMDI4NjUxMDA4.DSE0WA.wsmRJE604ux20WZBrfIToYPo4yk"
+    return ""
 
 
 def database_token():
     # This is just a placeholder key.
-    return "mongodb://whowoss:46Zmu6EYY5vvz65K@whowossdb-shard-00-00-p9a4a.mongodb.net:27017,whowossdb-shard-" \
-           "00-01-p9a4a.mongodb.net:27017,whowossdb-shard-00-02-p9a4a.mongodb.net:27017/test?ssl=true&replica" \
-           "Set=whowossdb-shard-0&authSource=admin"
+    return ""
